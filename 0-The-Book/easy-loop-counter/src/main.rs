713214ea77cc6591a90cc9d0cf6a0047f083bc1e
Kyle Martin <kyle.martin.0819@gmail.com>
2023-12-16 21:53:55 -0500

    Easy Loops
    Simple use of a loop that checks a condition to exit. Could've done with while, but wanted to use a loop.

---
+++ 0-The-Book/easy-loop-counter/src/main.rs
@@ -0,0 +1,15 @@
+fn main() {
+    let mut number = 0;
+    let ten_x_developer = 6969;
+
+    loop {
+        if number == ten_x_developer {
+            println!("The number is: {number}.");
+            println!("Officially a 10x Developer. Prime would be proud.");
+            break;
+        }
+
+        println!("The number is: {number}.");
+        number += 1;
+    }
+}

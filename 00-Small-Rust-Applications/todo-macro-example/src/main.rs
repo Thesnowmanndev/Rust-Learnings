aade6dd36e94f9db9c70de9c8feabcec72f4a38e
Kyle Martin <kyle.martin.0819@gmail.com>
2024-01-02 18:24:17 -0500

    Tested the todo macro to see the terminal result

---
+++ 00-Small-Rust-Applications/todo-macro-example/src/main.rs
@@ -0,0 +1,9 @@
+fn main() {
+    greeting();
+    println!("Hello, world!");
+}
+
+fn greeting() {
+    todo!("Hey! {}, this code needs to be finished.", "Kyle");
+    // TODO: Listen to the macro and code this.
+}

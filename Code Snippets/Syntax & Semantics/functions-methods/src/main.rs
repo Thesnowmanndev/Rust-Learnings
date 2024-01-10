4f9f59e46f395461f4468a2e38dc5d574d647008
Kyle Martin <kyle.martin.0819@gmail.com>
2024-01-09 19:34:45 -0500

    feat: fns

---
+++ Code Snippets/Syntax & Semantics/functions-methods/src/main.rs
@@ -0,0 +1,10 @@
+// This is a default example of a function
+fn main() {
+    println!("Hello, world!");
+}
+
+// Example of a public function that takes in two ints and returns one int
+pub fn add(x: i32, y: i32) -> i32{
+    let sum = x + y;
+    sum
+}
